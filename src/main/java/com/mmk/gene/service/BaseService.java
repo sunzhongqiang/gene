4e250bf64836b9bf94f87c4354e3b982d2b83104
sunzhongqiang <sunzhongqiang@lensung.com>
2020-07-25 09:12:08 +0800

    feat(升级版本): Pom中对spring boot的依赖从2.1升级到2.2.9

--- src/main/java/com/mmk/gene/service/BaseService.java
+++ src/main/java/com/mmk/gene/service/BaseService.java
@@ -6,17 +6,16 @@ import org.springframework.data.domain.Pageable;
 
 /**
  * 基础服务类，写了通用的方法，继承之后不需要每个类再写一遍.
- * 
- * @author 孙中强 sunzhongqiang
  *
- * @param <T> 实体类
+ * @param <T>  实体类
  * @param <ID> 主键
+ * @author 孙中强 sunzhongqiang
  */
 public interface BaseService<T, ID> {
 
     /**
      * 检查是否存在主键为id的实体.
-     * 
+     *
      * @param id 不能为空
      * @return 如果有返回true，其他为false
      * @throws IllegalArgumentException if {@code id} is {@literal null}
@@ -25,7 +24,7 @@ public interface BaseService<T, ID> {
 
     /**
      * 根据主键获得一个引用，注意并没有从数据库中获取数据.
-     * 
+     *
      * @param id 主键
      * @return 主键对应的对象引用
      */
@@ -33,7 +32,7 @@ public interface BaseService<T, ID> {
 
     /**
      * 从数据库中根据id获取一个实体.
-     * 
+     *
      * @param id 主键
      * @return 主键对应的实体对象
      */
@@ -41,7 +40,7 @@ public interface BaseService<T, ID> {
 
     /**
      * 保存或者更新一个实体.
-     * 
+     *
      * @param entity 要保存的对象
      * @return 持久化后的对象
      */
@@ -50,23 +49,23 @@ public interface BaseService<T, ID> {
 
     /**
      * 保存或者更新一组实体.
-     * 
+     *
      * @param entities 要保存的对象
-     * @param <S> 泛型对象
+     * @param <S>      泛型对象
      * @return 保存后的结果
      */
     <S extends T> List<S> save(Iterable<S> entities);
 
     /**
      * 根据主键进行删除数据.
-     * 
+     *
      * @param id 主键
      */
     void deleteById(ID id);
 
     /**
      * 批量删除一组实体.
-     * 
+     *
      * @param entities 要删除的实体
      */
     void deleteInBatch(Iterable<T> entities);
@@ -78,14 +77,14 @@ public interface BaseService<T, ID> {
 
     /**
      * 返回所有实体.
-     * 
+     *
      * @return 所有实体
      */
     Iterable<T> findAll();
 
     /**
      * 根据给定的主键返回所有.
-     * 
+     *
      * @param ids 主键s
      * @return 符合条件的一组结果集
      */
@@ -93,14 +92,14 @@ public interface BaseService<T, ID> {
 
     /**
      * 统计总数.
-     * 
+     *
      * @return 现在实体中的所有数量
      */
     long count();
 
     /**
      * 删除给定的实体.
-     * 
+     *
      * @param entity 要删除的实体
      * @throws IllegalArgumentException in case the given entity is {@literal null}.
      */
@@ -108,7 +107,7 @@ public interface BaseService<T, ID> {
 
     /**
      * 批量删除实体.
-     * 
+     *
      * @param entities 要删除的实体
      * @throws IllegalArgumentException in case the given {@link Iterable} is {@literal null}.
      */
@@ -121,7 +120,7 @@ public interface BaseService<T, ID> {
 
     /**
      * 分页返回所有的实体.
-     * 
+     *
      * @param pageable 分页参数
      * @return a page of entities 分页结果
      */
